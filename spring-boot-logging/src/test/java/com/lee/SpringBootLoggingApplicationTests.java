d0e2c753013c1b99574ceefe8904d9f718e6eecd
Lee <chill_lee@163.com>
2020-07-11 23:32:08 +0800

    spring-boot-logging 测试

--- spring-boot-logging/src/test/java/com/lee/SpringBootLoggingApplicationTests.java
+++ spring-boot-logging/src/test/java/com/lee/SpringBootLoggingApplicationTests.java
@@ -1,6 +1,8 @@
 package com.lee;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,15 @@ class SpringBootLoggingApplicationTests {
 
     @Test
     void contextLoads() {
+        //记录器
+        Logger logger = LoggerFactory.getLogger(getClass());
+
+        logger.trace("trace 日志");
+        logger.debug("debug 日志");
+        logger.info("info 日志");
+        logger.warn("warn 日志");
+        logger.error("error 日志");
+
     }
 
 }
