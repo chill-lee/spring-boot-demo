9f13fe6b8b19e800a17dfd30ec6536b4bf85c4c7
Lee <chill_lee@163.com>
2020-07-11 16:02:04 +0800

    spring-boot-config-autoconfig

---
+++ spring-boot-config-autoconfig/src/main/java/com/lee/SpringBootConfigAutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigAutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigAutoconfigApplication.class, args);
+    }
+
+}
