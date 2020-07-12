08e79bf3ad8adb6788a41f5990f89c1a9e4c423b
chill <chill_lee@163.com>
2020-07-12 14:21:48 +0800

    新建helloworld

--- spring-boot-web/src/main/java/com/lee/web/springBootHello.java
+++ spring-boot-web/src/main/java/com/lee/web/springBootHello.java
@@ -1,9 +1,17 @@
 package com.lee.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @author Lee
  */
+@Controller
 public class springBootHello {
+
+    @ResponseBody
+    @RequestMapping("/hello")
     public String hello(){
         return "hello";
     }
