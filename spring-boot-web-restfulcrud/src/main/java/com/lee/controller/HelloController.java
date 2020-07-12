1a2193467f78d0a3a2391b93556e54a926c51f95
chill <chill_lee@163.com>
2020-07-12 15:38:02 +0800

    新建主页

--- spring-boot-web-restfulcrud/src/main/java/com/lee/controller/HelloController.java
+++ spring-boot-web-restfulcrud/src/main/java/com/lee/controller/HelloController.java
@@ -1,8 +1,19 @@
 package com.lee.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @Author Administrator
  * @create 2020/7/12 11:12
  */
+@Controller
 public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        return "hello world";
+    }
 }
